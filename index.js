9b5f8132238635f3d47197261f6a3d8ea27398da
Brendan McDonald <Brendan McDonald>
2016-11-02 17:40:24 -0400

    added movies.html to search for movies and modified the server to allow for searching

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var express = require('express')
 var mdb = require('moviedb')('7227ee533c81a8acbb443c98ec625841')
 var app = express()
-var movieRes = '';
-  mdb.movieInfo({id: 666}, function(err, res){
-    movieRes = JSON.stringify(res)
-  })
-app.get('/', function(req, res){
-  res.send(movieRes)
-})
+app.use(express.static('public'))
 
+app.get('/:id', function(req, response){
+  var id = req.params.id
+  mdb.movieInfo({id: id}, function(err, res){
+    response.send(JSON.stringify(res))
+  })
+})
 
 app.listen(3000)
