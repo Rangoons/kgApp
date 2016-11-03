aeedc7ce80b343da724c6b9f35e95b3307797582
Brendan McDonald <bjm4477@rit.edu>
2016-11-03 18:14:51 -0400

    search functionality and getting popular movies added

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ var mdb = require('moviedb')('7227ee533c81a8acbb443c98ec625841')
 var app = express()
 app.use(express.static('public'))
 
-app.get('/:id', function(req, response){
-  var id = req.params.id
-  mdb.movieInfo({id: id}, function(err, res){
-    response.send(JSON.stringify(res))
+
+app.get('/:title', function(req, response){
+  var title = req.params.title
+  mdb.searchMovie({query: title}, function(err, res){
+    response.send((res))
   })
 })
-
+//get all of the popular movies
+app.get('/', function(req, response){
+  mdb.miscPopularMovies(function(err, res){
+    response.send(res)
+  })
+})
 app.listen(3000)
